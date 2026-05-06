81719ab2024273f8fc700abe895039223de431ca
GivenchyCodes <Givens.abraham@contractors.bie.com.kw>
2026-05-06 17:37:42 +0300

    chores: husky: Keep calm and commit

---
+++ index.test.js
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest';
+
+describe('Backend Environment Sanity Check', () => {
+  it('should pass true as true', () => {
+    expect(true).toBe(true);
+  });
+});
